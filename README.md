22805133e8efc17b7191336798996da6259cbacc
Kazi Rayhan <thisiskazi@gmail.com>
2022-08-02 22:08:41 +0600

    project initailized

---
+++ README.md
@@ -0,0 +1,6 @@
+##Laravel UI : https://github.com/laravel/ui
+
+#Step 1 : composer require laravel/ui
+#Step 2 : php artisan ui bootstrap --auth
+#step 3 : npm install 
+#step 4 : npm run dev
---
+++ resources/views/login.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+@section('content')
+<div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi officia ratione quas necessitatibus fuga nobis sed nesciunt est, minus non optio sapiente! Ullam blanditiis voluptatibus, repellendus deserunt corporis maxime vel praesentium quam nihil magnam culpa id accusantium quasi est a natus placeat rerum incidunt nesciunt beatae libero. Deserunt fugiat quam numquam iusto placeat assumenda obcaecati qui officiis, vitae doloremque beatae quasi, ducimus natus enim est. Quia, corrupti distinctio commodi at, deserunt facere recusandae aliquam sunt consequuntur fugit hic iste consectetur quidem inventore nam atque, eius quisquam harum repellendus porro. Eaque quod labore unde, officiis natus commodi rem ad ut exercitationem.</p>
+</div>
+@endsection
